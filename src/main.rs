c2c1d0344c8950b260f2bdcab1425d1634704570
Alexander Nookinov <anookinov@gmail.com>
2021-09-01 00:05:57 +0700

    add matching named variables

--- src/main.rs
+++ src/main.rs
@@ -71,6 +71,18 @@ fn main() {
         3 => println!("three"),
         _ => println!("anything"),
     }
+
+    // matching named variables
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(y) => println!("Matched, y = {:?}", y),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y= {:?}", x, y);
 }
 
 fn foo(x: i32) {
