11cd7610ec937b5dbd0b3535666547794acf902a
Alexander Nookinov <anookinov@gmail.com>
2021-09-03 18:28:50 +0700

    add underscores in multiple spaces within one pattern

--- src/main.rs
+++ src/main.rs
@@ -200,6 +200,14 @@ fn main() {
     }
 
     println!("setting is {:?}", setting_value);
+
+    let numbers = (2, 4, 8, 16, 32);
+
+    match numbers {
+        (first, _, third, _, fifth) => {
+            println!("Some numbers: {}, {}, {}", first, third, fifth)
+        }
+    }
 }
 
 fn foo(x: i32) {
