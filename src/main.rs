914f236269c365f4933120b3a522520dcca5123c
Alexander Nookinov <anookinov@gmail.com>
2021-09-03 18:27:19 +0700

    add ignoring parts of a value with a nested _

--- src/main.rs
+++ src/main.rs
@@ -186,6 +186,20 @@ fn main() {
     }
 
     foo(3, 4);
+
+    let mut setting_value = Some(5);
+    let new_setting_value = Some(10);
+
+    match (setting_value, new_setting_value) {
+        (Some(_), Some(_)) => {
+            println!("Can't overwrite an existing customized value");
+        }
+        _ => {
+            setting_value = new_setting_value;
+        }
+    }
+
+    println!("setting is {:?}", setting_value);
 }
 
 fn foo(x: i32) {
