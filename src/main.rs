160d87dfdf9bbacfe2510ecc657a75a2cfc69fbc
Alexander Nookinov <anookinov@gmail.com>
2021-09-03 21:21:12 +0700

    add extra conditionals with match guards

--- src/main.rs
+++ src/main.rs
@@ -253,6 +253,33 @@ fn main() {
     //         println!("Some numbers: {}", second)
     //     },
     // }
+
+    let num = Some(4);
+
+    match num {
+        Some(x) if x < 5 => println!("less than five: {}", x),
+        Some(x) => println!("{}", x),
+        None => (),
+    }
+
+    let x = Some(5);
+    let y = 10;
+
+    match x {
+        Some(50) => println!("Got 50"),
+        Some(n) if n == y => println!("Matched, n = {}", n),
+        _ => println!("Default case, x = {:?}", x),
+    }
+
+    println!("at the end: x = {:?}, y = {}", x, y);
+
+    let x = 4;
+    let y = false;
+
+    match x {
+        4 | 5 | 6 if y => println!("yes"), // (4 | 5 | 6) if y => ...
+        _ => println!("no"),
+    }
 }
 
 fn foo(x: i32) {
