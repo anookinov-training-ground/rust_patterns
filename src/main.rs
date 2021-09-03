16a43c2103677d3292ac1e3d14e50173d0649885
Alexander Nookinov <anookinov@gmail.com>
2021-09-03 18:21:51 +0700

    add ignoring an entire value with _

--- src/main.rs
+++ src/main.rs
@@ -180,6 +180,12 @@ fn main() {
     }
 
     let ((feet, inches), Point { x, y }) = ((3, 10), Point { x: 3, y: -10 });
+
+    fn foo(_: i32, y: i32) {
+        println!("This code only uses the y parameter: {}", y);
+    }
+
+    foo(3, 4);
 }
 
 fn foo(x: i32) {
